2b735338248684dadce9d829c383a68dcbfb1c7e
fiddyspence <chris.spence@puppetlabs.com>
2013-05-31 14:36:53 +0100

    Initial commit

---
+++ catalogcompile.ddl
@@ -0,0 +1,28 @@
+metadata    :name        => "Puppet catalog agent",
+            :description => "Ask a puppetmaster to compile a catalog via mcollective",
+            :author      => "fiddyspence",
+            :license     => "ASL2",
+            :version     => "1.0",
+            :url         => "https://github.com/fiddyspence/mco_catalog",
+            :timeout     => 180
+
+  action compile, :description => "compile a catalog" do
+    display :always
+    input :server,
+            :prompt      => "Name of the node to compile a catalog for",
+            :description => "Package to #{act}",
+            :type        => :string,
+            :validation  => '.',
+            :optional    => false,
+            :maxlength   => 255 
+
+        output :catalog,
+               :description => "The catalog",
+               :display_as  => "Catalog"
+
+        output :time,
+               :description => "Catalog compile duration",
+               :display_as  => "Time"
+
+  end
+
---
+++ catalogcompile.rb
@@ -0,0 +1,33 @@
+require 'puppet'
+require 'puppet/face'
+
+module MCollective
+  module Agent
+    class Catalogcompile<RPC::Agent
+      metadata :name        => "Puppet catalog agent",
+               :description => "Ask a puppetmaster to compile a catalog via mcollective",
+               :author      => "fiddyspence",
+               :license     => "ASL2",
+               :version     => "1.0",
+               :url         => "http://github.com/fiddyspence/mco_catalog",
+               :timeout     => 180
+
+      action "compile" do
+        validate :server, String
+
+        facts=Puppet::Face[:facts, :current].find(request[:server])
+        node=Puppet::Node.new(request[:server])
+        node.merge(facts.values)
+        t = Time.now.to_f
+        catalog=Puppet::Resource::Catalog.indirection.find(request[:server], :node => node)
+        u = Time.now.to_f
+        reply[:catalog] = catalog.to_pson
+        reply[:time] = u-t
+
+      end
+
+    end
+  end
+end
+
+# vi:tabstop=2:expandtab:ai:filetype=ruby

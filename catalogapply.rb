63c59486e18ec6d7e9cbb0fb130bd72fb5089db6
fiddyspence <chris.spence@puppetlabs.com>
2013-10-16 18:15:57 +0200

    adding catalogapply agent

--- catalogapply.rb
+++ catalogapply.rb
@@ -3,45 +3,18 @@ module MCollective
     class Catalogapply<RPC::Agent
 
       action "apply" do
-      foo = File.open('/tmp/thelog','w')
-       require 'puppet'
-       require 'puppet/application'
-       require 'puppet/configurer'
-       ::Puppet.settings.preferred_run_mode = :agent
-        unless ::Puppet.settings.app_defaults_initialized?
-          ::Puppet.settings.initialize_app_defaults(::Puppet::Settings.app_defaults_for_run_mode(::Puppet.run_mode))
-        end
-
-      foo.write "here\n"
-      foo.flush  
-      begin
-        catalog = ::Puppet::Resource::Catalog.convert_from(::Puppet::Resource::Catalog.default_format,request[:catalog])
-        catalog = ::Puppet::Resource::Catalog.pson_create(catalog) unless catalog.is_a?(::Puppet::Resource::Catalog)
-      rescue => detail
-         foo.write "fucking error tard\n"
-  
-        raise ::Puppet::Error, "Could not deserialize catalog from pson: #{detail}"
+    
+result = {:exitcode => nil,
+                 :output => "",
+       }
+       File.open('/tmp/foocatalog','w') { |f| f.write request[:catalog] }
+       cmd = Shell.new("/opt/puppet/bin/puppet apply --catalog /tmp/foocatalog", :stdout => result[:output], :stderr => result[:output])
+       cmd.runcommand
+       File.unlink('/tmp/foocatalog','w')
+       result[:exitcode] = cmd.status.exitstatus
+       reply[:status] = result[:exitcode]
+       reply[:result] = result[:output]
       end
-  
-      foo.write "here too\n"
-      foo.flush  
-      configurer = ::Puppet::Configurer.new
-      foo.write "#{configurer.inspect}\n"
-      foo.write Time.now.to_s
-      foo.flush  
-      foo.write "now fucking here fuck you fucker\n"
-      foo.flush  
-      final_catalog = catalog.to_ral
-      final_catalog.finalize
-      tard = configurer.run(:catalog => final_catalog, :pluginsync => false)
-      foo.write Time.now.to_s
-      foo.flush  
-      foo.write "and the result we got back was: #{tard.inspect}\n"
-      foo.flush  
-      foo.write "\n\n we got iright to the fucking end here\n\n"
-      foo.flush  
-      end
-
     end
   end
 end
